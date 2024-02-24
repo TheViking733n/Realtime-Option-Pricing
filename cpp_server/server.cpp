fa31e5654d0087684fb656b690950385bb22fd32
Anant Prakash Singh <anant733n@gmail.com>
2024-02-24 17:53:46 +0530

    Option model class blueprint

--- cpp_server/server.cpp
+++ cpp_server/server.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include "websocket.hpp"
 
+class OptionModel {
+public:    
+    OptionModel();
+    
+    ~OptionModel();
+    
+    double blackScholes(double S, double K, double r, double sigma, double T, char type);
+    
+    double binomial(double S, double K, double r, double sigma, double T, int steps, char type);
+    
+    double monteCarlo(double S, double K, double r, double sigma, double T, int simulations, char type);
+    
+    void calculateGreeks(double S, double K, double r, double sigma, double T, double &delta, double &gamma, double &theta, double &vega, double &rho, char type);
+};
+
 int main() {
     WebSocketServer server;
 
